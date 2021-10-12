00b65e26d96d1e4593f0cf85cfb6512347d47e5e
bodybody123 <bodybodyofficial@gmail.com>
2021-10-12 11:24:24 +0700

    add theme

---
+++ src/themes/MuiTheme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const font = "'Noto Serif', serif";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: font,
+    },
+});
+
+export default theme;
